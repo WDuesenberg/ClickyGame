8276e3dd6fad855e73264676bced7971b25df932
w_duse89 <wyatt.duesenberg@gmail.com>
2019-11-06 10:51:46 -0700

    stuff

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
-import FriendCard from "./component/FriendCard";
-import Wrapper from "./component/Wrapper";
-import friendsList from "./friends.json";
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"; // importing 'useState' to give us the hooks functionality
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friendList from "./friends.json"; // renamed to 'friendList' so there's no conflict w/our friends const below
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    // creating a React state value 'friends' and specifying the method we'll use to alter that state, 'updateFriends'
+    // we set these values up in the form of an array (order matters!) and then set the initial value for the state
+    // value ('friends', in this case) by calling the React method 'useState()' and passing it the initial value for
+    // that state...
+    const [friends, updateFriends] = useState(friendList);
+
+    const removeFriend = id => {
+        // 'friends' in our filter below is our value from React state
+        const remainingFriends = friends.filter(friend => friend.id !== id);
+        // now that we're using hooks, we use each state's custom update method to alter its value, much in the same
+        // way we formerly used 'this.setState()' to update state
+        updateFriends(remainingFriends);
+    }
+
+    return (
+        <Wrapper>
+            <h1 className="title">Friends List</h1>
+            {friends.map(friend => (
+                <FriendCard
+                    name={friend.name}
+                    image={friend.image}
+                    occupation={friend.occupation}
+                    location={friend.location}
+                    key={friend.id}
+                    removeFriend={() => removeFriend(friend.id)}
+                />
+            ))}
+        </Wrapper>
+    );
 }
 
 export default App;
